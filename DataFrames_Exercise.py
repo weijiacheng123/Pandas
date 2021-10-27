a8b17022e4e9dc3a5ff893e0eacf4ff213ce1e31
Weijia Cheng <weijia_cheng1@baylor.edu>
2021-10-26 19:17:54 -0500

    DataFrames_exercise

---
+++ DataFrames_Exercise.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+print("-----------------------------------------------   Question #1  -----------------------------------------------")
+print("1. Produce that had the highest and lowest sales in total sales (both name of produce and value)")
+
+
+print()
+print("-----------------------------------------------   Question #2  -----------------------------------------------")
+print("2. Using 'loc', display the quantity and total sales for 'Orange' and 'Beets' (together)")
+
+
+print()
+print("-----------------------------------------------   Question #3  -----------------------------------------------")
+print("3. Using 'loc', display the total sales for 'Apples' through 'Lettuce'")
+
+
+print()
+print("-----------------------------------------------   Question #4  -----------------------------------------------")
+print("4. Using 'at', update the quantity sold for Apricots to 11,955 and total sales to 44,353.05")
+
+
+print()
+print("-----------------------------------------------   Question #5  -----------------------------------------------")
+print("5. What is the average quantity sold across all products? (print out ONLY quantity sold)")
+
+
+print()
+print("-----------------------------------------------   Question #6  -----------------------------------------------")
+print("6. Create a new dataframe for only those produce that have sold between 11,500 to 12,000 (quantity)")
+
+
+print()
+print("-----------------------------------------------   Question #7  -----------------------------------------------")
+print("7. What is the total sales for the products in the above new dataframe? (print out ONLY total sales)")
+
+
+print()
